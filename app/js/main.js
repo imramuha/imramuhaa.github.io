f186ec73bc91ebcb2290b26fc9ffd039dc49f740
imranmuha <imramuha@student.arteveldehogeschool.be>
2017-05-25 19:15:50 +0200

    Changes and adjusments to several pages
    Algemen voorwaarden, Privacy en cookiebeleid en Contact pagina's zijn
    bijgewerkt

--- app/js/main.js
+++ app/js/main.js
@@ -2,15 +2,26 @@ $(document).ready(function() {
     $('#navigatie-opener').click(function() {
         $('body').toggleClass('geopend');
         $('header').toggleClass('geopend');
+        $('#portfolio').toggleClass('geopend');
         $('#sidenav').toggleClass('geopend');
 });
     $('#navigatie-dicht').click(function() {
          $('body').removeClass('geopend');
         $('header').removeClass('geopend');
+        $('#portfolio').removeClass('geopend');
         $('#sidenav').removeClass('geopend');
     });
 });
 
-
-
-// momenteel rond vid 5 17 mins about to make scrollable navigatie
+// voor google maps
+ function initMap() {
+        var uluru = {lat: 51.0673017, lng: 3.7004048};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
